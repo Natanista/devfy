68341b207167caae4e1c3a08edd6961e95048b43
rafaelluzetti <rafaelluzetti@hotmail.com>
2021-11-04 19:57:28 -0300

    Criado API.js para conexao axios

--- front-end/src/api.js
+++ front-end/src/api.js
@@ -2,9 +2,8 @@ import axios from 'axios'
 
 const api = axios.create(
     {
-        baseUrlr: "https://618324d791d76c00172d17c8.mockapi.io/music"
+        baseUrl: "https://61845f70ac0b850017489e1d.mockapi.io/devfy/dashboard"
     }
 );
-
 
 export default api;
