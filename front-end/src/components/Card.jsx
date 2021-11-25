0e648f323cef67ec43759cfd292728b324ee0694
GuilhermeMiranda1 <guilherme.miranda@bandtec.com.br>
2021-11-25 18:31:00 -0300

    Integrando dashboard

--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -10,7 +10,7 @@ function Card(props) {
       <img class="icone_dow_img" src={icon_dow} alt="icone de Download" />
        </div>
           <p>
-            <strong>Título: </strong>{props.projeto}
+            <strong>Título: </strong>{props.titulo}
           </p>
           
           <p>
@@ -30,11 +30,11 @@ function Card(props) {
           <p>
             <strong>Descrição: </strong>
           </p>
+
           <p class="card_descricao">
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi corporis porro veniam laboriosam obcaecati doloribus nihil, corrupti accusantium. Assumenda ratione facere omnis quos molestias, nulla odit totam suscipit cupiditate! Asperiores?
-       Expedita excepturi labore quod nihil explicabo magni quo, accusantium error voluptatem consequuntur cumque necessitatibus a voluptatum dolorum possimus eveniet deleniti facilis dignissimos. Fugit a saepe nisi et hic architecto soluta.
           {props.descricao}
           </p>
+
       <button class="btn_verde">Editar</button>
       <button class="btn_vermelho">Excluir</button>
       <p class="p_valor"><b> R$ 150 - 150</b></p>
--- front-end/src/pages/DashboardEmpresa.jsx
+++ front-end/src/pages/DashboardEmpresa.jsx
@@ -1,80 +1,99 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import MenuLogado from "../components/MenuLogado";
 import Footer from "../components/Footer";
 import icon_dow from '../html/img/baixar.png'
 import Card from "../components/Card";
 import '../html/css/dashboardEmpresa.css';
+import api from "../api";
 
+function Cards() {
+    const [cards, setCards] = useState([]); 
 
+    useEffect(() => {
+        async function buscarCards() {
+            const resposta = await api.get("/projetos")
+            setCards(resposta.data)
+            console.log("olha o que veio da api!!!", resposta.data)
+        }
+        buscarCards()
+    }, []);
 
-function DashboardEmpresa(){
-    return(
-<div>        
-    <div>
-        <MenuLogado/>
-    </div>
-        
-    <div class="div_fundo">
-        <div class="div_esquerda">
-            <div class="div_status_projeto">
-            <div class="conteudo_financeiro">
-            <h3>Status dos projetos:</h3>
-            <br />
-       <p><b>Projetos em aberto:</b> 3</p>
-         <p><b>Projetos em andamentos:</b>5</p>
-       <p><b>Projetos finalizados:</b>90</p>
-       <br />
-       <h3>Financeiro</h3>
-       <br />
-       <p><b>Dísponivel par resgate:</b> R$ 300.00</p>
-       <p><b>Contratos em andamento:</b>3</p>
-       <br />
-       <button> <b> Atualiza Financeiro</b> </button>
-     </div>
-            </div>
-
-            <div class="div_status_cards">
-                <div className="conteudo_status">
-                    <h3>Status dos cards</h3>
-                    <br />
-                    <ul>
-                    <li class="li_1">Aberto</li>
-                    <li class="li_1">Finalizado</li>
-                    <li class="li_1">Andamento</li>
-                    </ul>
+        return (
+            <div>
+                <div>
+                    <MenuLogado/>
                 </div>
-            </div>
-        </div>
 
-        <div class="div_direita">
-            <div class="div_icons">
+                <div className="div_fundo">
+                    <div className="div_esquerda">
+                        <div className="div_status_projeto">
+                            <div className="conteudo_financeiro">
+                                <h3>Status dos projetos:</h3>
+                                <br />
+                                <p><b>Projetos em aberto:</b> 3</p>
+                                <p><b>Projetos em andamentos:</b>5</p>
+                                <p><b>Projetos finalizados:</b>90</p>
+                                <br />
+                                <h3>Financeiro</h3>
+                                <br />
+                                <p><b>Dísponivel par resgate:</b> R$ 300.00</p>
+                                <p><b>Contratos em andamento:</b>3</p>
+                                <br />
+                                <button> <b> Atualiza Financeiro</b> </button>
+                            </div>
+                        </div>
 
-                <img class="icone_dow_img" src={icon_dow} alt="icone de Download" />
-                <p>Importar projeto</p>
-                <button>Cadastrar projeto</button>
-                <h2>Projetos</h2>
-                <select id="card" class="card">
-                    <option value="#">Selecione status do projeto</option>
-                    <option value="aberto">Aberto</option>
-                    <option value="finalizado">Finalizado</option>
-                    <option value="andamento">Andamento</option>
-                </select>
+                        <div className="div_status_cards">
+                            <div className="conteudo_status">
+                                <h3>Status dos cards</h3>
+                                <br />
+                                <ul>
+                                    <li className="li_1">Aberto</li>
+                                    <li className="li_1">Finalizado</li>
+                                    <li className="li_1">Andamento</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
 
-            </div>    
-            <div class="div_cards">
-                <Card/> 
-            </div>
-            
-        </div>
+                    <div className="div_direita">
+                        <div className="div_icons">
 
-    <br />
-    </div>
-    
-    <div>
-    <Footer/>
-    </div>
-</div>
-)
-}
+                            <img className="icone_dow_img" src={icon_dow} alt="icone de Download" />
+                            <button className="btn_cadastrar_projeto">Cadastrar projeto</button>
+                            <select id="card" className="dropdown_status">
+                                <option value="#">Selecione status do projeto</option>
+                                <option value="aberto">Aberto</option>
+                                <option value="finalizado">Finalizado</option>
+                                <option value="andamento">Andamento</option>
+                            </select>
 
-export default DashboardEmpresa;
+                        </div>
+                        <div className="div_cards">
+                            { 
+                                cards.map((card) => (
+                                    <Card
+                                        key={card.id}
+                                        id={card.id}
+                                        titulo={card.titulo}
+                                        empresa={card.empresa}
+                                        linguagem={card.linguagem}
+                                        tempoEstimado={card.tempoEst}
+                                        descricao={card.descricao}
+                                    />
+                                ))
+                            }
+                        </div>
+
+                    </div>
+
+                    <br />
+                </div>
+
+                <div>
+                    <Footer />
+                </div>
+                </div>
+        )
+    }
+export default Cards;
