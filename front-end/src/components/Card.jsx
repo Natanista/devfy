54544db8a04d3700c62dfa6c526bb56413441148
Isabela01vsilva <isabela.vsilva@bandtec.com.br>
2021-11-25 18:57:23 -0300

    Integração excluir projeto cadastrado

--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import icon_dow from '../html/img/baixar.png'
 import '../html/css/dashboardEmpresa.css'
+import api from "../api"
 
 function Card(props) {
+
+  function deletarMusica(){
+    api.delete(`/${props.id}`)
+    .then((resposta)=>{
+      window.location.reload();
+      console.log("DELETOU!!!")
+    })
+    .catch((erro) => {
+      console.log("ERRO AO DELETAR!")
+    })
+  }
+
   return (
     <div class="div_card_projetos">
       <div class="div_card_conteudo">
@@ -36,7 +49,9 @@ function Card(props) {
           {props.descricao}
           </p>
       <button class="btn_verde">Editar</button>
-      <button class="btn_vermelho">Excluir</button>
+
+      <button class="btn_vermelho" onClick={deletarMusica}>Excluir</button>
+
       <p class="p_valor"><b> R$ 150 - 150</b></p>
     </div>
    
