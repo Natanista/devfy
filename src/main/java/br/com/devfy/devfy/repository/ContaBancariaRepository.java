0d5d70f4d26ea9c69f3f494ccd3bfb68915b1c9b
natanista <natan.silva@bandtec.com.br>
2022-06-01 21:20:17 -0300

    logica do holder

--- src/main/java/br/com/devfy/devfy/repository/ContaBancariaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.devfy.devfy.repository;
-
-import br.com.devfy.devfy.entity.ContaBancaria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ContaBancariaRepository extends JpaRepository<ContaBancaria, Integer> {
-
-}
